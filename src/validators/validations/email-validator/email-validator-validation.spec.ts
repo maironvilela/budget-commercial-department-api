db95da949131afa935f903ed080acf79922ce280
Mairon Vilela <maironvilela@hotmail.com>
2022-02-12 20:22:24 -0300

    test: ensure EmailValidatorValidation return InvalidParamError if invalid email provided

--- src/validators/validations/email-validator/email-validator-validation.spec.ts
+++ src/validators/validations/email-validator/email-validator-validation.spec.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { faker } from '@faker-js/faker';
 
 import { EmailValidation } from '@/validators';
 import { EmailValidatorValidation } from '.';
+import { InvalidParamError } from '@/presentation';
 
 interface SutTypes {
   sut: EmailValidatorValidation;
@@ -30,5 +32,14 @@ describe('EmailValidatorValidation', () => {
     const error = sut.validate({ email: faker.internet.email() });
 
     expect(error).toBeUndefined();
+  });
+  it('Should EmailValidatorValidation return InvalidParamError if invalid email provided', () => {
+    const { sut, emailValidationStub } = makeSut();
+
+    jest.spyOn(emailValidationStub, 'isValid').mockReturnValueOnce(false);
+
+    const error = sut.validate({ email: 'invalid_email' });
+
+    expect(error).toEqual(new InvalidParamError('email'));
   });
 });
