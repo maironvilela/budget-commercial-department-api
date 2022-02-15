95e0fea4c507b83210f8fdb299d90572820e6f4c
Mairon Vilela <maironvilela@hotmail.com>
2022-02-15 18:34:00 -0300

    fix: Creation instance AccountTypeormRepository in DbAuthenticationFactory

--- src/main/factors/data/db-authentication-factory.ts
+++ src/main/factors/data/db-authentication-factory.ts
@@ -1,11 +1,13 @@
 import bcrypt from '@/config/bcrypt';
 import { DbAuthentication } from '@/data';
-import { BcryptAdapter, JWTAdapter } from '@/infra';
+import { AccountTypeormRepository, BcryptAdapter, JWTAdapter } from '@/infra';
 
 export const makeDbAuthentication = (): DbAuthentication => {
   const hashCompare = new BcryptAdapter(bcrypt.salt);
   const createAuth = new JWTAdapter();
-  const loadAccountByEmailRepository = null;
+
+  const loadAccountByEmailRepository = new AccountTypeormRepository();
+
   return new DbAuthentication(
     loadAccountByEmailRepository,
     hashCompare,
